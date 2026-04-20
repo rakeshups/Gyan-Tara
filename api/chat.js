849422c8fa0791862c1a428159dbc6eb5f42bab8
Rakesh Raidash <rakeshraidash.np@gmail.com>
2026-04-20 08:16:43 +0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -19,7 +19,7 @@ export default async function handler(req, res) {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": `Bearer ${process.env.VITE_GROQ_KEY}`,
+       "Authorization": `Bearer ${process.env.GROQ_KEY}`,
       },
       body: JSON.stringify({
         model: "llama-3.3-70b-versatile",
